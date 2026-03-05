b906de1d2cb5af98bab9c38826664ae3c305c268
Joca-pudim <121590506+Joca-pudim@users.noreply.github.com>
2026-03-05 10:54:03 -0300

    Delete 8. Deposito 1,3%.py

--- 8. Deposito 1,3%.py
+++
@@ -1,7 +0,0 @@
-# 8. Receba o valor de um depósito em poupança. Calcule e mostre o valor após 1 mês de aplicação sabendo que rende 1,3% a. m.
-
-deposito = float(input("Digite o valor do depósito: "))
-
-valor_final =  (deposito * 1.013)
-
-print("Valor após 1 mês: R$, {:.2f}".format(valor_final))
