34ec7d9dabbfbfd78c1a3abfebeaa55e3252aafb
Joca-pudim <121590506+Joca-pudim@users.noreply.github.com>
2026-03-05 10:52:35 -0300

    Delete 11. Raio Circunferência.py

--- 11. Raio Circunferência.py
+++
@@ -1,8 +0,0 @@
-# 11. Receba o raio de uma circunferência. Calcule e mostre o comprimento da circunferência.
-
-pi = 3.14
-raio = float(input("Digite o raio da circunferência: "))
-
-comprimento = 2 * pi * raio
-
-print("O comprimento da circunferência é: {:.2f}".format(comprimento))
