4d59b92599c1b633d714778e0270d2879a2fae9a
Joca-pudim <121590506+Joca-pudim@users.noreply.github.com>
2026-03-05 10:45:31 -0300

    Delete 9.Soma dos Quadrados.py

--- 9.Soma dos Quadrados.py
+++
@@ -1,8 +0,0 @@
-# 9. Receba os 2 números inteiros. Calcule e mostre a soma dos quadrados.
-
-n1 = int(input("Digite o primeiro número inteiro: "))
-n2 = int(input("Digite o segundo número inteiro: "))
-
-soma = n1**2 + n2**2
-
-print("A soma dos quadrados é:", soma)
