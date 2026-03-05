b56fb65c37b770a9a74d107b4b7aa609bc662db0
Joca-pudim <121590506+Joca-pudim@users.noreply.github.com>
2026-03-05 10:53:29 -0300

    Delete 3. Area Triangulo.py

--- 3. Area Triangulo.py
+++
@@ -1,8 +0,0 @@
-# 3. Receba a base e a altura de um triângulo. Calcule e mostre a sua área.
-
-base = float(input("Digite a base do triângulo: "))
-altura = float(input("Digite a altura do triângulo: "))
-
-area = (base * altura) / 2
-
-print("A área do triângulo é:", area)
