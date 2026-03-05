3c350404bc87b874de5b86eba42fc52712bb8ce9
Joca-pudim <121590506+Joca-pudim@users.noreply.github.com>
2026-03-05 10:52:18 -0300

    Delete 1. Area Quadrado.py

--- 1. Area Quadrado.py
+++
@@ -1,8 +0,0 @@
-# 1. Coletar o valor do lado de um quadrado, calcular sua área e apresentar o resultado.
-
-
-lado = float(input("Digite o valor do lado do quadrado: "))
-
-area = lado ** 2
-
-print("A área do quadrado é:", area)
