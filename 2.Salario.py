1505ba28d10ba214a2cfb3cb5df39cf862bff723
Joca-pudim <121590506+Joca-pudim@users.noreply.github.com>
2026-03-05 10:44:31 -0300

    Delete 2.Salario.py

--- 2.Salario.py
+++
@@ -1,8 +0,0 @@
-# 2. Receba o salário de um funcionário e mostre o novo salário com reajuste de 15%.
-
-salario = float(input("Digite o salário do funcionário: "))
-
-reajuste = salario * 0.15
-novo_salario = salario + reajuste
-
-print("Novo salário com reajuste de 15%:", novo_salario)
