35b2f1e2b214d15f41f1bac459d8d9c4d7549cd8
Joca-pudim <121590506+Joca-pudim@users.noreply.github.com>
2026-03-05 10:52:24 -0300

    Delete 10. Diferença de numeros.py

--- 10. Diferença de numeros.py
+++
@@ -1,8 +0,0 @@
-# 10. Receba 2 números reais. Calcule e mostre a diferença desses valores.
-
-n1 = float(input("Digite o primeiro número: "))
-n2 = float(input("Digite o segundo número: "))
-
-diferenca = n1 - n2
-
-print("A diferença entre o primeiro número para o segundo é:", diferenca)
